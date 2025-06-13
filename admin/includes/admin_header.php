73eebac3aa572d49566f2e9510b36499f10113fa
Zain Ul Abideen <zain65.apply@gmail.com>
2025-06-14 03:12:57 +0500

    admin fav icon

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -9,6 +9,7 @@ include '../includes/config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title><?= $page_title ?? 'Dashboard' ?> | Al-Hijrah Trust</title>
 
+    <link rel="icon" href="../../assets/images/trust-logo.png" type="image/png">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
