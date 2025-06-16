1196011c8f7b7662e1b5841a2db75c8845f3643a
Zain Ul Abideen <zain65.apply@gmail.com>
2025-06-16 14:04:52 +0500

    fav icon size

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
 
 
 
-     <link rel="stylesheet" href="/../assets/css/style.css">
+     <link rel="stylesheet" sizes="180x180" href="/../assets/css/style.css">
      
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
